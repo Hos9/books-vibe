ac5fda3248d7ccb01aecef606e799fd2febcb415
Hos9 <shahin9315@gmail.com>
2026-04-08 06:13:40 +0600

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className="bg-amber-600 text-center w-11/12 mx-auto text-3xl">
+        HomePage
+      </h1>
+    </div>
+  );
+};
+
+export default Homepage;
