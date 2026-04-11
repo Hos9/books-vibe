5d7335a9bb6d603c1f560aa7ceedfee58720fb2a
Hos9 <shahin9315@gmail.com>
2026-04-11 21:27:47 +0600

    39-8 module completed

---
+++ src/Component/ListedBooks/ListedWishList.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { BookContext } from "../../Context/BookCOntext";
+import BookCard from "../UI/BookCard";
+
+const ListedWishList = () => {
+  const { storedBook, wishList } = useContext(BookContext);
+  console.log(storedBook, wishList, "bookContext");
+
+  if (wishList.length === 0) {
+    return (
+      <div className="container mx-auto bg-base-200 text-center h-[50vh] flex items-center justify-center">
+        <h1 className="font-bold text-3xl">
+          No book(s) available in this list.
+        </h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+      {wishList.map((book, ind) => (
+        <BookCard key={ind} book={book} />
+      ))}
+    </div>
+  );
+};
+
+export default ListedWishList;
--- src/books/Books.jsx
+++ src/books/Books.jsx
@@ -1,13 +1,31 @@
 import React, { useContext } from "react";
 import { BookContext } from "../Context/BookCOntext";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import ListedReadList from "../Component/ListedBooks/ListedReadList";
+import ListedWishList from "../Component/ListedBooks/ListedWishList";
 
 const Books = () => {
-  const { storedBook } = useContext(BookContext);
-  console.log(storedBook, "bookContext");
+  const { storedBook, wishList } = useContext(BookContext);
+  console.log(storedBook, wishList, "bookContext");
 
   return (
-    <div className="container mx-auto">
-      <h1 className="">My Books</h1>
+    <div className="container mx-auto mt-10">
+      <br />
+
+      <Tabs>
+        <TabList>
+          <Tab>Read List</Tab>
+          <Tab>Wish List</Tab>
+        </TabList>
+
+        <TabPanel>
+          <ListedReadList />
+        </TabPanel>
+        <TabPanel>
+          <ListedWishList />
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
